8f2ce1464905bfb3768a0ccbef3702abff0f0bb1
prasadwa <prasad.wanigasinghe@ebuilder.com>
2017-06-22 09:07:47 +0530

    dashboard service0 .

---
+++ src/providers/dashboard-provider.ts
@@ -0,0 +1,88 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import config from '../app/config.json';
+
+
+/*
+  Generated class for the DashboardProvider provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class DashboardProvider {
+
+  numberOfPosts = 0;
+  numberOfUsers = 0;
+  numberOfFollowings = 0;
+  numberOfOwnPosts = 0;
+
+  constructor(public http: Http) {
+    console.log('Hello DashboardProvider Provider');
+  }
+
+  loadNumberOfPosts(){
+    if (this.numberOfPosts !== 0){
+      return Promise.resolve(this.numberOfPosts);
+    }
+
+    return new Promise(resolve => {
+      this.http.post(config.main.baseUrl + '/loadNumberOfPosts',{user_id:0})
+        .map(res => res.json())
+        .subscribe(data => {
+          this.numberOfPosts = data[0].species + data[0].complains;
+          resolve(this.numberOfPosts);
+        });
+    });
+  }
+
+
+  loadNumberOfUsers(){
+    if (this.numberOfUsers !== 0){
+      return Promise.resolve(this.numberOfUsers);
+    }
+
+    return new Promise(resolve => {
+      this.http.post(config.main.baseUrl + '/loadNumberOfUsers',{user_id:0})
+        .map(res => res.json())
+        .subscribe(data => {
+          this.numberOfUsers = data[0].users;
+          resolve(this.numberOfUsers);
+        });
+    });
+  }
+
+
+  loadNumberOfFollwings(id){
+    if (this.numberOfFollowings !== 0){
+      return Promise.resolve(this.numberOfFollowings);
+    }
+
+    return new Promise(resolve => {
+      this.http.post(config.main.baseUrl + '/loadNumberOfFollwings',{user_id:id})
+        .map(res => res.json())
+        .subscribe(data => {
+          this.numberOfFollowings = data[0].species + data[0].complains;
+          resolve(this.numberOfFollowings);
+        });
+    });
+  }
+
+
+  loadNumberOfOwnPosts(id){
+    if (this.numberOfPosts !== 0){
+      return Promise.resolve(this.numberOfPosts);
+    }
+
+    return new Promise(resolve => {
+      this.http.post(config.main.baseUrl + '/loadNumberOfOwnPosts',{user_id:id})
+        .map(res => res.json())
+        .subscribe(data => {
+          this.numberOfPosts = data[0].species + data[0].complains;
+          resolve(this.numberOfPosts);
+        });
+    });
+  }
+
+}
