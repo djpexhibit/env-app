b66fa7237973f1dc84d079a00a151b367f45e0d5
prasadwa <prasad.wanigasinghe@ebuilder.com>
2017-11-05 08:02:57 +0530

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform ,AlertController} from 'ionic-angular';
 import { StatusBar } from 'ionic-native';
 import { LoginPage } from '../pages/login/login';
 import { SplashScreen } from '@ionic-native/splash-screen';
+declare var FirebasePlugin: any;
+
 
 @Component({
   template: `<ion-nav [root]="rootPage"></ion-nav>`
@@ -10,12 +12,74 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 export class MyApp {
   rootPage = LoginPage;
 
-  constructor(platform: Platform, public _SplashScreen: SplashScreen) {
+  constructor(platform: Platform, public _SplashScreen: SplashScreen, public alertCtrl: AlertController) {
     platform.ready().then(() => {
       setTimeout(() => {
         this._SplashScreen.hide();
       }, 100);
       StatusBar.styleDefault();
+
+      FirebasePlugin.getToken(token => {
+        // save this server-side and use it to push notifications to this device
+        console.log(`Obtained token: ${token}`);
+        FirebasePlugin.subscribe('all');
+        let youralert = this.alertCtrl.create({
+          title: 'New Push notification',
+          message: 'vvvvvvvvvv'
+        });
+        youralert.present();
+      }, error => {
+        console.error(`Error: ${error}`);
+        let youralert = this.alertCtrl.create({
+          title: 'New Push notification',
+          message: 'gggggggggg'
+        });
+        youralert.present();
+      });
+
+      FirebasePlugin.onTokenRefresh(token => {
+        // save this server-side and use it to push notifications to this device
+        console.log(`Refreshed token: ${token}`);
+        let youralert = this.alertCtrl.create({
+          title: 'New Push notification',
+          message: '9999'
+        });
+        youralert.present();
+      }, function(error) {
+        console.error(`Error: ${error}`);
+        let youralert = this.alertCtrl.create({
+          title: 'New Push notification',
+          message: 'kkkkkkk'
+        });
+        youralert.present();
+      });
+
+
+      FirebasePlugin.onNotificationOpen(data => {
+        // check notification contents and react accordingly
+        console.log(JSON.stringify(data));
+        if(data.wasTapped){
+          let youralert = this.alertCtrl.create({
+            title: 'New Push notification',
+            message: data
+          });
+          youralert.present();
+        }else{
+          let youralert = this.alertCtrl.create({
+            title: 'New Push notification',
+            message: data
+          });
+          youralert.present();
+        }
+      }, function(error) {
+        console.error(`Error: ${error}`);
+        let youralert = this.alertCtrl.create({
+          title: 'New Push notification',
+          message: '5656'
+        });
+        youralert.present();
+      });
+
     });
   }
 }
